03ee95d51497823dd2ab5cf2a2e7037c1dc7299f
Ahvizra <73788885+Ahvizra@users.noreply.github.com>
2020-12-14 02:11:27 +0200

    goit-markup-hw-08

--- mobilemenu.js
+++ mobilemenu.js
@@ -1,4 +1,4 @@
-(() => {
+/* (() => {
   const refs = {
     openMobilBtn: document.querySelector('[mobile-open]'),
     closeMobilBtn: document.querySelector('[mobile-close]'),
@@ -11,4 +11,19 @@
   function toggleModal() {
     refs.mobil.classList.toggle('is-close');
   }
+})();
+ */
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("close-btn");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
